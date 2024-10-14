070425d61e8168c73b459e2702e5b1ad6e1537db
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-10-14 19:03:04 +0400

    TaskAllSide

---
+++ Pactice1/Server.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+//client ekrani 10 saniyeden bir screenshot edib servere gonderir.
+//server client uchun folder , onun ichirisine download edir.
+
+TcpClient listener;
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 64405;
+var endPoint = new IPEndPoint(ip, port);
+var server = new TcpListener(endPoint);
+try
+{
+    server.Start();
+    Console.WriteLine("Server Started...");
+    while (true)
+    {
+        listener = server.AcceptTcpClient();
+        if (listener != null)
+        {
+            Console.WriteLine($"{listener.Client.RemoteEndPoint} Connected.");
+            _ = Task.Run(() => AcceptClient(listener));
+        }
+        else Console.WriteLine("listener is Null!");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Have Problem: {ex.Message}");
+}
+
+static void AcceptClient(TcpClient client)
+{
+    var adresss = client.Client.RemoteEndPoint as IPEndPoint;
+    if (adresss != null)
+    {
+        string clientFolder = $"C:\\Users\\ASUS\\Desktop\\{adresss.Address}";
+        if (!Directory.Exists(clientFolder))
+        {
+            Directory.CreateDirectory(clientFolder);
+        }
+
+        using (var stream = client.GetStream())
+        {
+            while (true)
+            {
+                var lengthBuffer = new byte[5];
+                int bytesRead = stream.Read(lengthBuffer, 0, lengthBuffer.Length);
+                if (bytesRead == 0) break;
+
+                int length = BitConverter.ToInt32(lengthBuffer, 0);
+                var buffer = new byte[length];
+
+                int totalRead = 0;
+                while (totalRead < length)
+                {
+                    int read = stream.Read(buffer, totalRead, length - totalRead);
+                    if (read == 0) break;
+                    totalRead += read;
+                }
+
+                var path = Path.Combine(clientFolder, $"screenshot_{DateTime.Now:HH.mm.ss}.png");
+                using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    fileStream.Write(buffer, 0, length);
+                }
+                Console.WriteLine($"Photo Saved: {path}");
+            }
+        }
+    }
+    else Console.WriteLine("Adress Can not be Null");
+    client.Close();
+}
---
+++ Practic/Client.cs
@@ -0,0 +1,78 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+
+var a = 0;
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 64405;
+var ep = new IPEndPoint(ip, port);
+var client = new TcpClient();
+
+try
+{
+    client.Connect(ep);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Server Connected.");
+    Console.WriteLine("Press Any Key For Cancel...And Wait 10 Second\n");
+    Console.ResetColor();
+    //while (a < 5)
+    //{
+    //    TakeScreenShotAndSend(client);
+    //    a++;
+    //    Thread.Sleep(5000);
+    //}
+    ConsoleKeyInfo rK = new ConsoleKeyInfo();
+    var cancellationSource = new CancellationTokenSource();
+    var cancellationToken = cancellationSource.Token;
+    _ = Task.Run(() =>
+    {
+        rK = Console.ReadKey();
+        cancellationSource.Cancel();
+    });
+
+    while (true)
+    {
+        if (cancellationToken.IsCancellationRequested) // threadsleep olduguna gore 10 saniye gozdedmek lazimci cancel etmek ucun
+        {
+            Console.WriteLine($"You Pressed {rK.Key}\nProcess Ended");
+            client.Close();
+            client.Dispose();
+            break;
+        }
+
+        var task = Task.Run(() => { TakeScreenShotAndSend(client, cancellationToken); });
+        Thread.Sleep(10000);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    client.Close();
+}
+
+static void TakeScreenShotAndSend(TcpClient client, CancellationToken t)
+{
+    using (var bitmap = new Bitmap(1920, 1080))
+    using (var gBitMap = Graphics.FromImage(bitmap))
+    {
+        gBitMap.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+        using (var ms = new MemoryStream())
+        {
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            byte[] imageBytes = ms.ToArray();
+
+            var lengthBytes = BitConverter.GetBytes(imageBytes.Length);
+            var networkStream = client.GetStream();
+
+            networkStream.Write(lengthBytes, 0, lengthBytes.Length);
+            networkStream.Write(imageBytes, 0, imageBytes.Length);
+            networkStream.Flush();
+        }
+        t.ThrowIfCancellationRequested();
+
+        Console.WriteLine($"ScreenShot Sended");
+    }
+}
